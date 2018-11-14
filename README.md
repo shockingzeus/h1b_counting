58fd8b8ab68804d8e14d79a271ac0cecc46f2a72
shockingzeus <33131506+shockingzeus@users.noreply.github.com>
2018-11-13 23:46:22 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# h1b_counting
+
+
+# Problem
+ 
+As defined in https://github.com/InsightDataScience/h1b_statistics:
+
+"A newspaper editor was researching immigration data trends on H1B(H-1B, H-1B1, E-3) visa application processing over the past years, trying to identify the occupations and states with the most number of approved H1B visas. She has found statistics available from the US Department of Labor and its [Office of Foreign Labor Certification Performance Data](https://www.foreignlaborcert.doleta.gov/performancedata.cfm#dis). But while there are ready-made reports for [2018](https://www.foreignlaborcert.doleta.gov/pdf/PerformanceData/2018/H-1B_Selected_Statistics_FY2018_Q4.pdf) and [2017](https://www.foreignlaborcert.doleta.gov/pdf/PerformanceData/2017/H-1B_Selected_Statistics_FY2017.pdf), the site doesn’t have them for past years."
+
+This python 3 code aims to solve the problem and provides report for any year with available data. The input takes the form of .csv files downloaded from the Google drive [folder](https://drive.google.com/drive/folders/1Nti6ClUfibsXSQw5PUIWfVGSIrpuwyxf?usp=sharing). The outputs are two files under the output folder:
+
+* `top_10_occupations.txt`: Top 10 occupations for certified visa applications
+* `top_10_states.txt`: Top 10 states for certified visa applications
+
+As stated in https://github.com/InsightDataScience/h1b_statistic:
+
+"Each line holds one record and each field on each line is separated by a semicolon (;).
+
+Each line of the `top_10_occupations.txt` file should contain these fields in this order:
+1. __`TOP_OCCUPATIONS`__: Use the occupation name associated with an application's Standard Occupational Classification (SOC) code
+2. __`NUMBER_CERTIFIED_APPLICATIONS`__: Number of applications that have been certified for that occupation. An application is considered certified if it has a case status of `Certified`
+3. __`PERCENTAGE`__: % of applications that have been certified for that occupation compared to total number of certified applications regardless of occupation. 
+
+The records in the file must be sorted by __`NUMBER_CERTIFIED_APPLICATIONS`__, and in case of a tie, alphabetically by __`TOP_OCCUPATIONS`__.
+
+Each line of the `top_10_states.txt` file should contain these fields in this order:
+1. __`TOP_STATES`__: State where the work will take place
+2. __`NUMBER_CERTIFIED_APPLICATIONS`__: Number of applications that have been certified for work in that state. An application is considered certified if it has a case status of `Certified`
+3. __`PERCENTAGE`__: % of applications that have been certified in that state compared to total number of certified applications regardless of state.
+
+The records in this file must be sorted by __`NUMBER_CERTIFIED_APPLICATIONS`__ field, and in case of a tie, alphabetically by __`TOP_STATES`__."
+
+# Approach
+
+Each data file is associated with an instance of a custom class "h1b_report". We use the built-in cvs module to load the data, and use the header (first line) to determine the index for the relevant column:  the "status" (whether the application was certified or not), "soc_code" (code for occupation), "soc_name" (name of occupation) and the "state". We then iterate through and parse each line (application) to extract the corresponding values in these columns. 
+
+For all the "certified" applications, we first check if the entry for the "soc_code" or "state" is valid; invalid data are stored in "dumps" for further examination. Since our output has the precision on the first significant digit, warnings messages are raised if invalid data exceeds 0.1\% of the total "certified" applications. During my test I found the invalid entries were but a tiny fraction of all data, which did not seems to affect our output. 
+
+For all the valid values, we use two python dictionaries to update the number of applications associated with each unique "soc_code" or "state", in which the key is "soc_code" or "state" and the value the number of relevant applications. For the `top_10_states.txt` output it is straightforward: after we iterate through all the data, we simply sort the state / application number dictionary values and pick the top 10. For  `top_10_occupations.txt` there is one extra step, as there could be multiple variations of "soc_name"  associated with an unique "soc_code". We therefore check the occurrence of different "soc_name" for each "soc_code", and choose the most frequently used "soc_name" for each "soc_code". This allows us to sort first the application number then the "soc_name". 
+
+# Note
+
+ . This version does not support .xlsx file format available on the US Department of Labor website. Normally to handle .xlsx file we could use the xlrd module, but it is not a build-in one for python, so I am not sure if it is allowed in this challenge.
+ . As we know, python's float datatype does not always round percentage as intended, for instance, sometimes 0.5 round to 0. This is fairly trivial, but for precision we use decimal datatype instead. 
+ . It took ~7 seconds to process a ~200 Mbyte data on a Macbook pro.  
+ 
+# Run
+
+Simply run the bash script: bash run.sh
+
+
+ 
+
---
+++ insight_testsuite/temp/src/h1b_counting.py
@@ -0,0 +1,160 @@
+
+# coding: utf-8
+
+# In[199]:
+
+import sys
+import csv
+import re
+from decimal import Decimal
+
+
+# In[93]:
+
+states=['AK','AL','AR','AS','AZ','CA','CO','CT','DC','DE','FL','GA','GU','HI','IA','ID','IL','IN','KS','KY','LA','MA','MD',
+ 'ME','MI','MN','MO','MP','MS','MT','NA','NC','ND','NE','NH','NJ','NM','NV','NY','OH','OK', 'OR','PA','PR','RI','SC',
+ 'SD','TN','TX','UT','VA','VI','VT','WA','WI','WV','WY']
+
+
+# In[200]:
+
+class h1b_report:
+    def __init__(self, file):
+    # Initialize, load the data file. Find the relevant column from the headers (first line in file).
+        self.f = open(file,newline = "") 
+        self.reader = csv.reader(self.f, delimiter = ";",skipinitialspace = True )
+        self.headers = next(self.reader)
+        self.soc_index = self.find_index('SOC','CODE')
+        self.name_index = self.find_index('SOC','NAME')
+        self.status_index = self.find_index('STATUS')
+        self.state_index = self.find_index('WORK','STATE')
+        self.soc_report = dict() 
+        self.state_report = dict()
+        self.name_report = dict()
+        self.pattern = re.compile(r'^\d{2}-\d{4}')# The soc_code should follow "XX-XXXX" format.
+        self.typo_state = [] # Dump for invalid data with possible typos in the "STATE" column.
+        self.typo_soc = [] # Dump for invalid data with possible typos in the "SOC_CODE" column.
+        
+    def find_index(self,*args):
+        for i,header in enumerate(self.headers):
+            if all(arg in header for arg in args):
+                return i
+        
+    def process(self):
+    # Process the data. Invalid data (probably due to typos) were filtered out and saved as lists for examination. 
+    # Keep track of the number of the invalid data. If it is more than 0.1%, raise a warning.
+        typo_count_state = 0
+        typo_count_soc = 0
+        total_count = 0
+        for line in self.reader:
+            if len(line)==1:
+                line = line[0].rsplit(";")
+            if line[self.status_index].rstrip().lower()=='certified':
+                total_count+=1
+                soc = line[self.soc_index]
+                soc = self.valid_soc(soc)
+                name = line[self.name_index].rstrip()
+                state = line[self.state_index]
+                if state not in states:
+                    self.typo_state.append(line)
+                    typo_count_state+=1
+                elif not soc:
+                    self.typo_soc.append(line)
+                    typo_count_soc+=1
+                else:
+                    self.update_report(soc, self.soc_report)
+                    self.update_report(state, self.state_report)
+                    self.update_name(soc, name)
+                    
+        if total_count == 0:
+            print("Warning - no certified record in this file.")
+        else:
+            if typo_count_state/total_count>0.001:
+                print("Warning - {1} out of {2} records have errors in the STATE column.".format(typo_count_state,total_count))
+            if typo_count_soc/total_count>0.001:
+                print("Warning - {1} out of {2} records have errors in the SOC_CODE column.".format(typo_count_soc,total_count))
+                   
+    def update_report(self,tag, report_dict):
+        if not tag:
+            return
+        elif tag not in report_dict:
+            report_dict[tag]=1
+        else:
+            report_dict[tag]+=1  
+            
+    def update_name(self, soc, name):
+    # Note that a single SOC_CODE may correspond to quite a few variations of SOC_NAME. 
+    # Some of the SOC_NAME entry might even be empty - we refer it as "No Value".
+    # So we keep track of the occurrence of SOC_NAME associated with SOC_CODE. 
+    # We choose the SOC_NAME that occurs most frequently as the "default" SOC_NAME for that particular SOC_CODE. 
+    # We do not consider the occurrence of "No Value" unless we have no other choice.
+        name = name.strip("\"")
+        if not name:
+            name = "No Value"
+        if soc not in self.name_report:
+            self.name_report[soc] = {name:0} if name == "No Value" else {name:1}
+        elif name not in self.name_report[soc]:
+            new_item = {name:0} if name == "No Value" else {name:1}
+            self.name_report[soc].update(new_item)
+        else:
+            addto = 0 if name == "No Value" else 1
+            self.name_report[soc][name]+=addto
+            
+    def soc_to_name(self, soc):
+    # get the SOC_NAME that occurs most frequently as the "default" SOC_NAME for that particular SOC_CODE.  
+        names = self.name_report[soc]
+        return max(names.keys(), key=(lambda key: names[key])) 
+
+    
+    def valid_soc(self, soc):
+    # Check if the soc_code follows the right format. Convert soc_code to "\d\d-\d\d\d\d" if possible.
+    # Invalid soc will return None. The associated data would be put in the dump for examination if necessary.
+        if self.pattern.match(soc):
+            if len(soc.rstrip()) ==7:
+                return soc
+            elif soc[7] == ".":
+                return soc[:7]
+            else: 
+                return None
+        
+    def top_state(self, output):
+        total_num = sum(self.state_report.values())
+        topstates = sorted(self.state_report.items(), key = lambda x:(-x[1], x[0]))
+        with open(output,"w") as output:
+            output.write(";".join(["TOP_STATES","NUMBER_CERTIFIED_APPLICATIONS","PERCENTAGE"])+"\n")
+            for i in range(min(10,len(topstates))):
+                state, num = topstates[i]
+                ratio = Decimal(num/total_num)
+                output.write(";".join([state, str(num), "{:.1%}".format(ratio)])+"\n")
+                
+    def top_soc(self, output):
+        total_num = sum(self.soc_report.values())
+        to_sort = [(self.soc_to_name(soc), value) for soc, value in self.soc_report.items()]
+        top_soc = sorted(to_sort, key = lambda x:(-x[1], x[0]))
+        with open(output,"w") as output:
+            output.write(";".join(["TOP_OCCUPATIONS","NUMBER_CERTIFIED_APPLICATIONS","PERCENTAGE"])+"\n")
+            for i in range(min(10,len(top_soc))):
+                name, num = top_soc[i]
+                ratio = Decimal(num/total_num)
+                output.write(";".join([name, str(num), "{:.1%}".format(ratio)])+"\n")
+                
+    def close(self):
+        if self.f:
+            self.f.close()
+            self.f = None   
+            
+def main(source, output1, output2):  
+    import contextlib
+    with contextlib.closing(h1b_report(source)) as report:
+        report.process()
+        report.top_soc(output1)
+        report.top_state(output2)
+    
+if __name__=="__main__":
+    main(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
+
+
+# In[ ]:
+
+
+
